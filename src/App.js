f03c51921ece788aa069de2eb7343f7141d74361
Sean Schmidt <schmidtsean048@gmail.com>
2021-05-05 13:22:00 -0600

    pushing to production

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 import SeanResume from './images/SeanResume.jpeg';
 
 function App() {
   return (
     <div className="App">
-      <img src={SeanResume} />
+      <img src={SeanResume} alt='resume' />
     </div>
   );
 }
